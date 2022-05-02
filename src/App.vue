aeab598ff28512caeab016e052362a74826bea81
Jaime Jonathan Ortiz Velazco <jonathan.ortiz.velazco@gmail.com>
2022-05-02 11:34:46 -0500

    se agrego admitancia y susceptancia

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,21 @@
       <div class="col-9 col-md-9">
         <div class="mx-auto ">
           <smith-chart>
-            <sm-res-circle
+            <!-- <sm-res-circle
               :res="1"
               fill="rgba(0,0,255,0.5)"
               stroke="orange"
               stroke-width="2"
-            ></sm-res-circle>
+            ></sm-res-circle> -->
             <!-- Carga -->
-            <g fill="none" stroke-width="3" :stroke="stroke">
-              <circle :cx="radius" :cy="radius" :r="r" />
+            <g fill="none" stroke-width="3" :stroke="CargaDisplay">
+              <circle :cx="radius" :cy="radius" :r="r" :strohe="CargaDisplay" />
               <sm-point
                 :res="point.res"
                 :react="point.react"
-                fill="blue"
+                :fill="CargaDisplay"
               ></sm-point>
-              <g stroke="blue">
+              <g :stroke="CargaDisplay">
                 <line
                   :x1="radius"
                   :y1="radius"
@@ -28,8 +28,12 @@
               </g>
             </g>
             <!-- Coeficiente de reflexion -->
-            <sm-point-cr :h="p.h" :angle="p.angle" fill="green"></sm-point-cr>
-            <g stroke="green" stroke-width="3">
+            <sm-point-cr
+              :h="p.h"
+              :angle="p.angle"
+              :fill="CoeficienteDisplay"
+            ></sm-point-cr>
+            <g :stroke="CoeficienteDisplay" stroke-width="4">
               <line
                 :x1="radius"
                 :y1="radius"
@@ -41,9 +45,9 @@
             <sm-point
               :res="imped.res"
               :react="imped.react"
-              fill="red"
+              :fill="ImpedanciaDisplay"
             ></sm-point>
-            <g stroke="red" stroke-width="3">
+            <g :stroke="ImpedanciaDisplay" stroke-width="3">
               <line
                 :x1="radius"
                 :y1="radius"
@@ -56,15 +60,35 @@
             <sm-point
               :res="pointAdmitancia.res"
               :react="pointAdmitancia.react"
-              fill="purple"
+              :fill="AdmitanciaDisplay"
             ></sm-point>
-            <g stroke="purple">
+            <g :stroke="AdmitanciaDisplay" stroke-width="1  ">
               <line
                 :x1="radius"
                 :y1="radius"
                 :x2="getXAdmitancia"
                 :y2="getYAdmitancia"
               />
+            </g>
+            <!-- Stub -->
+            <g :stroke="StubDisplay" stroke-width="3">
+              <circle
+                :cx="radius + stubPoint.x"
+                :cy="radius + stubPoint.y"
+                :r="5"
+                :fill="StubDisplay"
+              />
+              <line :x1="radius" :y1="radius" :x2="getXStub" :y2="getYStub" />
+            </g>
+            <!-- Suceptancia -->
+            <g :stroke="SucepDisplay" stroke-width="3">
+              <circle
+                :cx="radius + sucebPoint.x"
+                :cy="radius + sucebPoint.y"
+                :r="5"
+                :fill="SucepDisplay"
+              />
+              <line :x1="radius" :y1="radius" :x2="getXSuceb" :y2="getYSuceb" />
             </g>
           </smith-chart>
         </div>
@@ -141,6 +165,7 @@
             <input
               v-model.number="er"
               type="number"
+              min="1"
               class="form-control"
               id="inlineFormInput"
               placeholder="0"
@@ -151,41 +176,60 @@
           </button>
         </div>
         <div class="card mb-4">
-          <div class="card-header d-flex justify-content-between">
-            <div :style="[circleRepre, { 'background-color': 'blue' }]"></div>
-            <h4>Localizar carga</h4>
-            <button
-              @click="localizarCarga"
-              type="button"
-              class="btn btn-primary"
-            >
-              Localizar
-            </button>
+          <div class="card-header ">
+            <div class="d-flex justify-content-between">
+              <div :style="[circleRepre, { 'background-color': 'blue' }]"></div>
+              <h4>Localizar carga</h4>
+              <button
+                @click="localizarCarga"
+                type="button"
+                class="btn btn-primary"
+              >
+                Localizar
+              </button>
+            </div>
+            <div class="d-flex align-items-center">
+              <input type="checkbox" @click="estadoCarga" />
+              Ocultar
+            </div>
           </div>
         </div>
         <div class="card mb-4">
-          <div class="card-header d-flex justify-content-between">
-            <div :style="[circleRepre, { 'background-color': 'green' }]"></div>
-            <h4>Coeficiente de reflexion</h4>
-            <button @click="addPoint" type="button" class="btn btn-primary">
-              Calcular
-            </button>
+          <div class="card-header ">
+            <div class="d-flex justify-content-between">
+              <div
+                :style="[circleRepre, { 'background-color': 'green' }]"
+              ></div>
+              <h4>Coeficiente de reflexion</h4>
+              <button @click="addPoint" type="button" class="btn btn-primary">
+                Calcular
+              </button>
+            </div>
+            <div class="d-flex align-items-center">
+              <input type="checkbox" @click="estadoCoeficiente" />
+              Ocultar
+            </div>
           </div>
           <p>{{ coeficiente }}</p>
         </div>
         <div class="card mb-4">
-          <div class="card-header d-flex justify-content-between">
-            <div :style="[circleRepre, { 'background-color': 'red' }]"></div>
-            <h4>Impedancia de entrada Z<sub>i</sub></h4>
-            <button @click="impedancia" type="button" class="btn btn-primary">
-              {{ accionImpedancia }}
-            </button>
+          <div class="card-header">
+            <div class=" d-flex justify-content-between">
+              <div :style="[circleRepre, { 'background-color': 'red' }]"></div>
+              <h4>Impedancia de entrada Z<sub>i</sub></h4>
+              <button @click="impedancia" type="button" class="btn btn-primary">
+                {{ accionImpedancia }}
+              </button>
+            </div>
+            <div class="d-flex align-items-center">
+              <input type="checkbox" @click="estadoImpedancia" />
+              Ocultar
+            </div>
           </div>
           <p>{{ impedanciaEntrada }}</p>
         </div>
         <div class="card mb-4">
           <div class="card-header d-flex justify-content-between">
-            <!-- <div :style="[circleRepre, { 'background-color': 'red' }]"></div> -->
             <h4>Longitud de la linea</h4>
             <button
               @click="longitudLinea"
@@ -205,15 +249,47 @@
             </button>
           </div>
           <div class="d-flex align-items-center">
-            <div :style="[circleRepre, { 'background-color': 'purple' }]"></div>
-            Admitancia
+            <div class="d-flex align-items-center">
+              <div
+                :style="[circleRepre, { 'background-color': 'purple' }]"
+              ></div>
+              Admitancia
+            </div>
+            <div class="d-flex align-items-center">
+              <input class="ms-3" type="checkbox" @click="estadoAdmitancia" />
+              Ocultar
+            </div>
           </div>
-
+          <div class="d-flex align-items-center">
+            <div class="d-flex align-items-center">
+              <div
+                :style="[circleRepre, { 'background-color': 'orange' }]"
+              ></div>
+              Stub
+            </div>
+            <div class="d-flex align-items-center">
+              <input class="ms-3" type="checkbox" @click="estadoStub" />
+              Ocultar
+            </div>
+          </div>
+          <div class="d-flex align-items-center">
+            <div class="d-flex align-items-center">
+              <div :style="[circleRepre, { 'background-color': 'cyan' }]"></div>
+              Susceptancia
+            </div>
+            <div class="d-flex align-items-center">
+              <input class="ms-3" type="checkbox" @click="estadoSucep" />
+              Ocultar
+            </div>
+          </div>
           <p>Longitud: {{ longitudStub }}</p>
           <p>Distancia a la carga: {{ distanciaStubCarga }}</p>
         </div>
       </div>
     </div>
+    {{ beta }}
+    <hr />
+    {{ betaPrima }}
   </div>
 </template>
 <script>
@@ -319,6 +395,22 @@ export default {
       let rad = this.rad(this.cxAdmitancia, this.cyAdmitancia);
       return 500 * (this.cyAdmitancia / rad) + this.radius;
     },
+    getXStub: function() {
+      let rad = this.rad(this.stubPoint.x, this.stubPoint.y);
+      return 500 * (this.stubPoint.x / rad) + this.radius;
+    },
+    getYStub: function() {
+      let rad = this.rad(this.stubPoint.x, this.stubPoint.y);
+      return 500 * (this.stubPoint.y / rad) + this.radius;
+    },
+    getXSuceb: function() {
+      let rad = this.rad(this.sucebPoint.x, this.sucebPoint.y);
+      return 500 * (this.sucebPoint.x / rad) + this.radius;
+    },
+    getYSuceb: function() {
+      let rad = this.rad(this.sucebPoint.x, this.sucebPoint.y);
+      return 500 * (this.sucebPoint.y / rad) + this.radius;
+    },
     r: function() {
       let r = Math.sqrt(Math.pow(this.cx, 2) + Math.pow(this.cy, 2));
       return r;
@@ -341,12 +433,17 @@ export default {
     return {
       impedanciaCarac: 0,
       impedanciaCarga: 0,
+      angle: 0,
+      reactCalculated: 0,
       impedanciaEntrada: "",
       point: { res: 1, react: 0 },
       imped: { res: 1, react: 0 },
       pointAdmitancia: { res: 1, react: 0 },
       p: { h: 1, angle: 0 },
+      pAdmitancia: { h: 1, angle: 0 },
       l: 0,
+      stubPoint: { x: 0, y: 0 },
+      sucebPoint: { x: 0, y: 0 },
       angulocarga: 0,
       anguloimpentrada: 0,
       frecuencia: 0,
@@ -355,9 +452,15 @@ export default {
       distanciaStubCarga: "",
       er: 1,
       beta: 0,
+      betaPrima: 0,
       coeficiente: "",
       radius: 400,
-
+      CoeficienteDisplay: "green",
+      CargaDisplay: "blue",
+      ImpedanciaDisplay: "red",
+      AdmitanciaDisplay: "purple",
+      StubDisplay: "orange",
+      SucepDisplay: "cyan",
       circleRepre: {
         width: "10px",
         height: "10px",
@@ -376,6 +479,7 @@ export default {
       this.localizarCarga();
       this.impedancia();
       this.longitudLinea();
+      this.stub();
     },
     addPoint() {
       let coef = this.pCoef();
@@ -493,10 +597,91 @@ export default {
         this.longLinea = "" + (angulolinea * 0.5) / 360 + " λ";
       }
     },
+    longitudAdmitanciaStub() {
+      let angulocarga =
+        ((math.acos(this.cx / this.rad(this.cx, this.cy)) * 180) / math.pi) *
+        (-math.abs(this.cy) / this.cy);
+      this.angulocarga = angulocarga;
+      let anguloimpentrada =
+        ((math.acos(this.cxImped / this.rad(this.cxImped, this.cyImped)) *
+          180) /
+          math.pi) *
+        (-math.abs(this.cyImped) / this.cyImped);
+      this.anguloimpentrada = anguloimpentrada;
+      let angulolinea = 0;
+      if (angulocarga < 0) {
+        if (anguloimpentrada > angulocarga) {
+          angulolinea = 360 + angulocarga - anguloimpentrada;
+        } else {
+          angulolinea = angulocarga - anguloimpentrada;
+        }
+      } else {
+        if (angulocarga > anguloimpentrada) {
+          angulolinea = angulocarga - anguloimpentrada;
+        } else {
+          angulolinea = 360 + angulocarga - anguloimpentrada;
+        }
+      }
+      if (this.frecuencia > 0) {
+        this.longLinea =
+          "" +
+          ((angulolinea * 0.5) / 360) *
+            (this.lambdaCero() / math.sqrt(this.er)) +
+          " m";
+      } else {
+        this.longLinea = "" + (angulolinea * 0.5) / 360 + " λ";
+      }
+    },
     stub() {
       let yl = math.divide(1, math.divide(this.carga(), this.impedanciaCarac));
       this.pointAdmitancia.res = yl.re;
       this.pointAdmitancia.react = yl.im;
+      let radCarga = this.rad(this.cx, this.cy);
+      let radUnitario = this.radius / 2;
+      let B = math.acos(
+        (math.pow(radUnitario, 2) -
+          math.pow(radCarga, 2) -
+          math.pow(radUnitario, 2)) /
+          (-2 * radCarga * radUnitario)
+      );
+      this.B = (B * 180) / math.pi;
+      this.stubPoint.x = radCarga * math.cos(B);
+      this.stubPoint.y = -radCarga * math.sin(B);
+      let react = -math.sqrt(
+        math.divide(-4 * this.stubPoint.x, this.stubPoint.x - this.radius)
+      );
+      this.reactCalculated = react;
+      let ycircleReact = this.radius / react;
+      let radiusReact = this.radius / math.abs(react);
+      let disCenterReact = math.sqrt(
+        math.pow(-this.radius, 2) + math.pow(-ycircleReact, 2)
+      );
+      // let disPuntReact = math.sqrt(
+      //   math.pow(-this.radius, 2) + math.pow(-this.radius - ycircleReact, 2)
+      // );
+      // let beta = math.acos(
+      //   (math.pow(disPuntReact, 2) -
+      //     math.pow(this.radius, 2) -
+      //     math.pow(disCenterReact, 2)) /
+      //     (-2 * this.radius * disCenterReact)
+      // );
+      let betaPrima = math.acos(
+        (math.pow(radiusReact, 2) -
+          math.pow(this.radius, 2) -
+          math.pow(disCenterReact, 2)) /
+          (-2 * this.radius * disCenterReact)
+      );
+      let alfa = math.asin(radiusReact / disCenterReact);
+      let angle = ((betaPrima + alfa) * 180) / math.pi;
+      if (angle <= 90) {
+        this.sucebPoint.y =
+          this.radius * math.sin(((180 - angle) * math.pi) / 180);
+        this.sucebPoint.x =
+          -this.radius * math.cos(((180 - angle) * math.pi) / 180);
+      } else {
+        this.sucebPoint.y = this.radius * math.sin((angle * math.pi) / 180);
+        this.sucebPoint.x = this.radius * math.cos((angle * math.pi) / 180);
+      }
     },
     pCoef() {
       let carga = this.carga();
@@ -514,6 +699,26 @@ export default {
     },
     rad(cx, cy) {
       return math.sqrt(cx * cx + cy * cy) + 1;
+    },
+    estadoCarga() {
+      this.CargaDisplay = this.CargaDisplay == "blue" ? "none" : "blue";
+    },
+    estadoCoeficiente() {
+      this.CoeficienteDisplay =
+        this.CoeficienteDisplay == "green" ? "none" : "green";
+    },
+    estadoImpedancia() {
+      this.ImpedanciaDisplay = this.ImpedanciaDisplay == "red" ? "none" : "red";
+    },
+    estadoAdmitancia() {
+      this.AdmitanciaDisplay =
+        this.AdmitanciaDisplay == "purple" ? "none" : "purple";
+    },
+    estadoStub() {
+      this.StubDisplay = this.StubDisplay == "orange" ? "none" : "orange";
+    },
+    estadoSucep() {
+      this.SucepDisplay = this.SucepDisplay == "cyan" ? "none" : "cyan";
     },
   },
 };
